60bcc95505bc9ff968dfc48c95315c37c2ba2b1e
Ejayz <32452006+Ejayz@users.noreply.github.com>
2024-09-12 00:13:42 +0800

    Updated One Fixing Cookies

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -12,19 +12,18 @@ export async function updateSession(request: NextRequest) {
   const supabase = createServerClient(PROJECT_URL, ANON_PUBLIC, {
     cookies: {
       getAll() {
-        return request.cookies.getAll()
+        return request.cookies.getAll();
       },
       setAll(cookiesToSet) {
-        cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
+        cookiesToSet.forEach(({ name, value, options }) =>
+          request.cookies.set(name, value)
+        );
         supabaseResponse = NextResponse.next({
           request,
-        })
+        });
         cookiesToSet.forEach(({ name, value, options }) =>
-          supabaseResponse.cookies.set(name, value, {
-            ...options,
-            maxAge: 3600,
-          })
-        )
+          supabaseResponse.cookies.set(name, value, options)
+        );
       },
     },
   });
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -15,10 +15,7 @@ export function createClient() {
       setAll(cookiesToSet) {
         try {
           cookiesToSet.forEach(({ name, value, options }) =>
-            cookieStore.set(name, value, {
-              ...options,
-              maxAge: 3600,
-            })
+            cookieStore.set(name, value, options)
           );
         } catch {
           // The `setAll` method was called from a Server Component.
