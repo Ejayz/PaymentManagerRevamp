32c0398d74554a45a0d9b86568bf75e2c7e40e57
Ejayz <32452006+Ejayz@users.noreply.github.com>
2024-09-12 00:37:57 +0800

    Update Fix Error
    Fix error where update will fail

--- app/api/v1/ops/updatetoken/route.ts
+++ app/api/v1/ops/updatetoken/route.ts
@@ -1,24 +1,18 @@
+import { createClient } from "@/utils/supabase/server";
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { createClient } from "@supabase/supabase-js";
-import { cookies } from "next/headers";
-
-const PROJECT_URL = process.env.PROJECT_URL || "";
-const ANON_PUBLIC = process.env.ANON_PUBLIC || "";
 
 export async function POST(req: NextRequest) {
   const { token_id, currency_name, currency_code } = await req.json();
-  const supabase = createClient(PROJECT_URL, ANON_PUBLIC);
-  const auth = cookies().get("auth");
-  const user = await supabase.auth.getUser(auth?.value);
-
+  const supabase = createClient();
+  const user = await supabase.auth.getUser();
   const { data, error } = await supabase
     .from("tbl_token")
     .update({ currency_name, currency_code })
     .eq("id", token_id)
     .eq("user_id", user.data.user?.id);
 
-    console.log(data, error);
+  console.log(data, error);
   if (error) {
     return NextResponse.json({ error: error.message }, { status: 400 });
   } else {
